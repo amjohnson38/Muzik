e69b03e16a24e24d328bcfa12c05ea5749500ff9
Angela Johnson <angiemjohnson95@gmail.com>
2017-08-22 19:58:30 -0400

    update path for background image

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-title">Music
+                <div className="App-title">Muzik
                 </div>
                 <FormGroup>
                     <InputGroup>
